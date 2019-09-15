785d1c54db14fbd15859d0d0b7615437bd1e6c41
Zak Saylors <zsaylors11@gmail.com>
2019-09-15 06:45:08 -0600

    Finished project with bug fixed.

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -12,6 +12,7 @@ public class MakeChangeApp {
 		paid = kb.nextDouble();
 
 		changeCalc(charged, paid);
+		changeCalc1(charged, paid);
 
 		kb.close();
 	}
@@ -21,81 +22,76 @@ public class MakeChangeApp {
 				nickel = 5, penny = 1;
 		double difference = (paid - charged) * 100 + 0.5;
 
-		System.out.print("Amount: " + charged + ", Tendered: " + paid + ", Result: ");
+		System.out.printf("Amount %.2f, Tendered: %.2f, Result: ", charged, paid);
 
 		if (difference < 1) {
 			System.out.println("Error!");
 		}
 
-		for (int i = 1; difference > 1; i++) {
+		while (difference > 1) {
 			if (difference / twentyDollars > 1) {
 				if (difference / twentyDollars < 2) {
 					System.out.print((int) difference / twentyDollars + " twenty dollar bill");
-					difference = difference % twentyDollars;
 				} else {
 					System.out.print((int) difference / twentyDollars + " twenty dollar bills");
-					difference = difference % twentyDollars;
 				}
+				difference = difference % twentyDollars;
 			} else if (difference / tenDollars > 1) {
 				if (difference / tenDollars < 2) {
 					System.out.print((int) difference / tenDollars + " ten dollar bill");
-					difference = difference % tenDollars;
 				} else {
-					System.out.print((int) difference / twentyDollars + " ten dollar bills");
-					difference = difference % twentyDollars;
+					System.out.print((int) difference / tenDollars + " ten dollar bills");
 				}
+				difference = difference % tenDollars;
 			} else if (difference / fiveDollars > 1) {
 				if (difference / fiveDollars < 2) {
 					System.out.print((int) difference / fiveDollars + " five dollar bill");
-					difference = difference % fiveDollars;
 				} else {
 					System.out.print((int) difference / fiveDollars + " five dollar bills");
-					difference = difference % fiveDollars;
 				}
+				difference = difference % fiveDollars;
 			} else if (difference / oneDollar > 1) {
 				if (difference / oneDollar < 2) {
 					System.out.print((int) difference / oneDollar + " one dollar bill");
-					difference = difference % oneDollar;
 				} else {
 					System.out.print((int) difference / oneDollar + " one dollar bills");
-					difference = difference % oneDollar;
 				}
+				difference = difference % oneDollar;
 			} else if (difference / quarter > 1) {
 				if (difference / quarter < 2) {
 					System.out.print((int) difference / quarter + " quarter");
-					difference = difference % quarter;
+
 				} else {
 					System.out.print((int) difference / quarter + " quarters");
-					difference = difference % quarter;
 				}
+				difference = difference % quarter;
 			} else if (difference / dime > 1) {
 				if (difference / dime < 2) {
 					System.out.print((int) difference / dime + " dime");
-					difference = difference % dime;
 				} else {
 					System.out.print((int) difference / dime + " dimes");
-					difference = difference % dime;
 				}
+				difference = difference % dime;
 			} else if (difference / nickel > 1) {
 				if (difference / nickel < 2) {
 					System.out.print((int) difference / nickel + " nickel");
-					difference = difference % nickel;
 				} else {
 					System.out.print((int) difference / nickel + " nickels");
-					difference = difference % nickel;
 				}
+				difference = difference % nickel;
 			} else {
 				if (difference / penny < 2) {
 					System.out.print((int) difference / penny + " penny");
-					difference = difference % penny;
 				} else {
 					System.out.print((int) difference / penny + " pennies");
-					difference = difference % penny;
 				}
+				difference = difference % penny;
 			}
 
 			if (difference >= 1) {
 				System.out.print(", ");
+			} else {
+				System.out.print(".");
 			}
 		}
 	}
