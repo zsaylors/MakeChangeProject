1c5b76f2eac09eb4c09f58793408bebc51b770fe
Zak Saylors <zsaylors11@gmail.com>
2019-09-15 07:01:55 -0600

    Fixed formatting

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -22,22 +22,24 @@ public class MakeChangeApp {
 		// AMMOUNT IS DOLLAR AND CHANGE AMOUNT MULTIPLIED BY 100.
 		int[] amount = new int[] { 10000, 5000, 2000, 1000, 500, 100, 25, 10, 5, 1 };
 		// CHANGE TYPES REPRESENT DOLLAR AMMOUNTS ASSOCIATED WITH INT[] AMMOUNT
-		String[] singularChangeType = new String[] { "one hundred dollar bill", "fifty dollar bill", "twenty dollar bill", "ten dollar bill", 
-													 "five dollar bill", "one dollar bill", "quarter", "dime", "nickel", "penny" };
-		String[] pluralChangeType = new String[] { "one hundred dollar bills", "fifty dollar bill", "twenty dollar bills", "ten dollar bills", 
-												   "five dollar bills", "one dollar bills", "quarters", "dimes", "nickels", "pennies" };
+		String[] singularChangeType = new String[] { "one hundred dollar bill", "fifty dollar bill",
+				"twenty dollar bill", "ten dollar bill", "five dollar bill", "one dollar bill", "quarter", "dime",
+				"nickel", "penny" };
+		String[] pluralChangeType = new String[] { "one hundred dollar bills", "fifty dollar bill",
+				"twenty dollar bills", "ten dollar bills", "five dollar bills", "one dollar bills", "quarters", "dimes",
+				"nickels", "pennies" };
 		double difference = (paid - charged) * 100 + 0.5;
 		int i = 0;
 
 		// FIRST PART OF PRINTOUT
 		System.out.printf("Amount %.2f, Tendered: %.2f, Result: ", charged, paid);
 
-		//ERROR MESSAGE IF PAID IS LESS THAN OWED.
+		// ERROR MESSAGE IF PAID IS LESS THAN OWED.
 		if (difference < 1) {
 			System.out.printf("Error! %.2f is owed.", (charged - paid));
 		}
 
-		//LOOP TO CALCULATE AND PRINT CHANGE.
+		// LOOP TO CALCULATE AND PRINT CHANGE.
 		for (i = 0; difference > 1; i++) {
 			double equation = difference / amount[i];
 			if (equation > 1) {
@@ -55,7 +57,7 @@ public class MakeChangeApp {
 				}
 			}
 		}
-		
+
 	}
-	
+
 }
